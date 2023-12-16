f225a54d5a3790be4dc4b5a4631a9cbd773162d7
Touisni <77310619+Touisni@users.noreply.github.com>
2023-12-16 01:44:41 +0100

    end

--- server.js
+++ server.js
@@ -1,62 +1,28 @@
 const express = require('express');
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
 const path = require('path');
-
-const app = express();
 
 const connectDB = require('./server/database/connection');
 
-dotenv.config({path:'views/.env'});
+dotenv.config({ path: 'views/.env' });
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-//log requests
-
 app.use(morgan('tiny'));
-
-//mongoDB connection
-
 connectDB();
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
 
-//parse request to body-parser
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use('/', require('./server/routes/router'));
 
-//set view engine
-
-app.set("view engine","ejs");
-
-//app.set("views",path.resolve(__dirname,"views/ejs"))
-
-//load assets
-app.use('/css', express.static (path.resolve(__dirname,"assets/css")));
-app.use('/img', express.static (path.resolve(__dirname,"assets/img")));
-app.use('/js', express.static (path.resolve(__dirname,"assets/js")));
-
-//css/style.css
-
-/*app.get('/', (req,res) => {
-//    res.render('index');
-//});
-
-//app.get('/add-user', (req,res) => {
-   // res.render('add_user');
-//});
-
-//app.get('/update-user', (req,res) => {
-  //  res.render('update_user');
-//});*/
-
-
-//load routers
-
-app.use('/',require('./server/routes/router'))
-
-app.listen(PORT,()=>{ 
-    console.log('server is running');
+app.listen(PORT, () => {
+    console.log('Server is running');
 });
