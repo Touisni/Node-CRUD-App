25bee1d9b421fd91bcef55da607e833c7d367aa0
Touisni <77310619+Touisni@users.noreply.github.com>
2023-12-16 01:23:25 +0100

    cleaned up

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const route = express.Router();
+
 const services = require('../services/render');
 const controller = require('../controller/controller');
 
-route.get('/', services.home);
-route.get('/add-user', services.addUser);
-route.get('/update-user', services.updateUser);
+route.get('/', services.homeRoutes);
+route.get('/add-user', services.add_user); 
+route.get('/update-user', services.update_user); 
 
 route.post('/api/users', controller.create);
 route.get('/api/users', controller.find);
